86370e640bc0b2abc16aad3d1da648dd51857e17
brifeb <brifeb@gmail.com>
2021-02-17 11:46:31 +0700

    update option

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-import requests, json
+import requests, json, glob
 from bs4 import BeautifulSoup
+import pandas as pd
 
 session = requests.Session()
 
@@ -45,11 +46,9 @@ def get_urls(page):
 
 
 def get_detail(url_detail):
-    print('get details..')
+    print(f'get details.. {url_detail}')
 
     url = "http://0.0.0.0:5000" + url_detail
-    print(url)
-
     res = session.get(url)
 
     # f = open('./res.html', 'w+')
@@ -72,35 +71,48 @@ def get_detail(url_detail):
         'description' : description
     }
 
-    print(dict_data)
-
     with open(f"./result{url_detail}.json", "w") as outfile:
         json.dump(dict_data,outfile)
 
 
 def create_csv():
-    print('csv generated..')
 
+    files = sorted(glob.glob('./result/*.json'))
+
+    datas = []
+    for file in files:
+        with open(file) as json_file:
+            data = json.load(json_file)
+            datas.append(data)
+
+    df = pd.DataFrame(datas)
+    df.to_csv('result.csv', index=False)
+
+    print('csv generated..')
 
 def run():
     total_pages = login()
 
-    # total_urls = []
-    # for i in range(total_pages):
-    #     page = i +1
-    #     urls = get_urls(page)
-    #     total_urls += urls
-    # with open('all_urls.json', 'w') as outfile:
-    #     json.dump(total_urls, outfile)
+    options = int(input("Input option number:\n1. Collecting all urls\n2. Get detail all products\n3. create csv\n"))
 
-    with open('all_urls.json') as json_file:
-        all_url = json.load(json_file)
+    if options == 1:
+        total_urls = []
+        for i in range(total_pages):
+            page = i +1
+            urls = get_urls(page)
+            total_urls += urls
+        with open('all_urls.json', 'w') as outfile:
+            json.dump(total_urls, outfile)
 
-    print(all_url[0])
-    for url in all_url:
-        get_detail(url)
+    if options == 2:
+        with open('all_urls.json') as json_file:
+            all_url = json.load(json_file)
 
-    create_csv()
+        for url in all_url:
+            get_detail(url)
+
+    if options == 3:
+        create_csv()
 
 
 if __name__ == '__main__':
